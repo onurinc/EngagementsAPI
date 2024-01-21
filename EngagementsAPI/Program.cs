1457e7db4b0df1b4a77c62beb99a045d673f3777
onurinc <onur.i@hotmail.nl>
2024-01-21 19:07:05 +0100

    rabbitmq consumer working

--- EngagementsAPI/Program.cs
+++ EngagementsAPI/Program.cs
@@ -1,7 +1,38 @@
 using EngagementsAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "mypass",
+    VirtualHost = "/"
+};
+
+var conn = factory.CreateConnection();
+
+using var channel = conn.CreateModel();
+
+channel.QueueDeclare("delete_user", durable: true, exclusive: false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume("delete_user", true, consumer);
+
 
 // Add services to the container.
 
@@ -9,6 +40,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 //var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 //var dbName = Environment.GetEnvironmentVariable("DB_NAME");
